cd1400e2f3ecb5e8024c0e7c97d7a680c151da23
Christopher-Shea <ctshea1@gmail.com>
2019-02-24 23:42:08 -0600

    findIntersection

---
+++ linkedList/findIntersection/findIntersection_README.md
@@ -0,0 +1,23 @@
+# Description
+Write a program to find the intersecton point of two linked lists and return a reference to that node.
+
+Given the following lists:
+
+```
+{a -> b -> c -> d -> 
+                     e -> f -> g}
+     {j -> k -> l ->
+```
+
+return {e -> f -> g}
+
+## Constraints
+If the lists do not intersect, return null.
+
+## Input
+Two singly-linked lists
+
+## Output
+The node where the input lists intersect
+
+## Additional Notes
---
+++ linkedList/findIntersection/findIntersection_solution.js
@@ -0,0 +1,78 @@
+const LinkedList = require('../linkedList.js').LinkedList;
+
+
+function findIntersection(list1, list2) {
+  // count length of lists
+  let length1 = 0
+  let length2 = 0;
+  let counter = list1.head;
+  while(counter){
+      length1++;
+      counter = counter.next;
+  }
+  counter = list2.head;
+  while(counter){
+      length2++;
+      counter = counter.next;
+  }
+  // determine which list is longer and normalize
+  let greater = length1 > length2 ? list1.head : list2.head;
+  let lesser = length1 > length2 ? list2.head : list1.head;
+  let difference = Math.abs(length1 - length2);
+  while (difference) {
+      greater = greater.next;
+      difference--;
+  }
+  // move through lists together until intersection is found
+  while (greater && lesser) {
+      if (greater === lesser) {
+          return greater;
+      } else {
+          greater = greater.next;
+          lesser = lesser.next;
+      }
+  }
+  // or end of lists is reached without finding intersection
+  return null;
+}
+
+// finds intersection
+let list1 = new LinkedList();
+let list2 = new LinkedList();
+for (let i = 1; i < 15; i++) {
+  list1.addTail(i);
+}
+for (let i = 46; i < 65; i++) {
+  list2.addTail(i);
+}
+console.log(list1.head.value); // 1
+console.log(list1.tail.value); // 14
+console.log(list2.head.value); // 46
+console.log(list2.tail.value); // 64
+let sharedTail = new LinkedList();
+for (let i = 100; i < 150; i++) {
+  sharedTail.addTail(i);
+}
+console.log(sharedTail.head.value); // 100
+console.log(sharedTail.tail.value); // 149
+let intersectionPoint = sharedTail.head;
+list1.tail.next = intersectionPoint;
+list2.tail.next = intersectionPoint;
+console.log(list1.tail.next === list2.tail.next); // true
+list1.tail = sharedTail.tail;
+list2.tail = sharedTail.tail;
+console.log(list1.tail === list2.tail); // true
+console.log(list1.tail.value); // 149
+console.log(list2.tail.value); // 149
+console.log(findIntersection(list1, list2) === intersectionPoint); // true
+
+// no intersection
+list1 = new LinkedList();
+list2 = new LinkedList();
+for (let i = 1; i < 15; i++) {
+  list1.addTail(i);
+}
+for (let i = 46; i < 65; i++) {
+  list2.addTail(i);
+}
+console.log(findIntersection(list1, list2)); // null
