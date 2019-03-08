6fb4fe4acd620fd960f5595949b53fbe3457cf68
Christopher-Shea <ctshea1@gmail.com>
2019-03-08 15:49:16 -0600

    hasAnagram comments

--- strings/hasAnagram/hasAnagram_solution.js
+++ strings/hasAnagram/hasAnagram_solution.js
@@ -1,30 +1,40 @@
 function hasAnagram(string, anagram) {
+  // set up library with charater counts for anagram
   let hash = {};
   for (let char of anagram) {
     hash[char] = hash[char] + 1 || 1;
   }
+  // toMatch will be used to ascertain when all characters(and their counts) of the anagram have been matched
   let toMatch = Object.keys(hash).length;
   let start = 0;
   let end = 0;
+  // use a sliding window as long as the target anagram
   while(end < anagram.length){
     let char = string[end];
+    // as each character in the string is seen
     if (char in hash) {
+      // if the character is in the anagram, subtract 1 from that character's count in the library
+      // (there is one less ocurrence of this character to match)
       hash[char] -= 1;
+      // if the library count hits zero, all occurrences of that character have been matched, update toMatch
       if (hash[char] === 0) {
         toMatch--;
       }
+      // if the library count hits negative zero, there are now more characters in the window than in the anagram
+      // update toMatch accordingly
       if (hash[char] === -1) {
         toMatch++;
       }
     }
     end++;
   }
-
+  // check if the initial window is already a target anagram
   if (toMatch === 0) {
     return true;
   }
-
+  // otherwise, slide the window down the string
   while(end < string.length){
+    // account for the new end character and update toMatch if necessary
     let newChar = string[end];
     if (newChar in hash) {
       hash[newChar] -= 1;
@@ -35,6 +45,7 @@ function hasAnagram(string, anagram) {
         toMatch++;
       }
     }
+    // account for the loss of the old start character and update toMatch if necessary
     let oldChar = string[start];
     if (oldChar in hash) {
       hash[oldChar] += 1;
@@ -45,6 +56,7 @@ function hasAnagram(string, anagram) {
         toMatch++;
       }
     }
+    // check if the current window is a target anagram
     if (toMatch === 0) {
       return true;
     }
@@ -55,7 +67,9 @@ function hasAnagram(string, anagram) {
 }
 
 
+console.log(hasAnagram('doesnotmatter', '')); // true
 console.log(hasAnagram('abcdefghi', 'zzzz')); // false
 console.log(hasAnagram('interesting', 'rent')); // true
 console.log(hasAnagram('algorithms', 'hit')); // true
 console.log(hasAnagram('notquite', 'nosir')); // false
+console.log(hasAnagram('exactmatch', 'hctamtcaxe')); // true
